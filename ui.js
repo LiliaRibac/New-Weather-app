ee4382b7c86e54857e79e02df7c51dfd466e88dd
LiliaRibac <ribac.lilia@gmai.com>
2019-09-09 11:54:29 -0700

    fix ui.js paint()

--- ui.js
+++ ui.js
@@ -24,16 +24,4 @@ class UI {
     this.feelsLike.textContent = `Wind Direction: ${windDirectionFromDegree(weather.wind.deg)}`
   }
   
-
-
-//  paint(weather) {
-//   this.location.textContent = `${weather.name}, ${weather.sys.country}`;
-//   this.desc.textContent = weather.weather[0].main;
-//   this.string.textContent = `${convertKelvinToCelsius(weather.main.temp)}\u00B0C`;
-
-//   this.humidity.textContent = `Humidity: ${weather.main.humidity}%`
-//   this.pressure.textContent = `Pressure: ${weather.main.pressure}hPa`
-//   this.windSpeed.textContent = `Wind Speed: ${MetresPerSecondToMilesPerHour(weather.wind.speed)} mph`
-//   this.windDirection.textContent = `Wind Direction: ${windDirectionFromDegrees(weather.wind.deg)}`
-// }
 }
